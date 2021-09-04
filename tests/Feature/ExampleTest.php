b3754f5a91949e994863a111904033f6ed2b29fc
Alex Rafuse <hey@alexrafuse.com>
2021-09-04 13:15:07 -0700

    relationship scaffolding

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,39 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
+use App\Models\Retailer;
+use App\Models\Stock;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    /** @test */
+    public function it_checks_stock_products_at_retailers()
+    {
+
+        $switch = Product::create(['name' => 'Nintendo Switch']);
+        $bestBuy = Retailer::create(['name' => 'Best Buy']);
+
+        $this->assertFalse($switch->inStock());
+
+        $stock = new Stock([
+            'price' => 10000,
+            'url' => 'http://alexrafuse.com/thing',
+            'sku' => '12345',
+            'in_stock' => true,
+        ]);
+
+        $bestBuy->addStock($switch, $stock);
+
+        $this->assertTrue($switch->inStock());
     }
 }
